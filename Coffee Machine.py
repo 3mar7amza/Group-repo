9792ab70b7139812e78b9e9b59ab184ed1ee9458
3mar7amza <omarabdelrahmanmohumed@gmail.com>
2026-08-08 13:11:42 +0300

    Done it

--- Coffee Machine.py
+++ Coffee Machine.py
@@ -5,17 +5,12 @@ class Resorces:
         self.Milk = Milk
         self.Coffee = Coffee
         self.Money = Money
-<<<<<<< HEAD
     def dict_func(self):
         Resorces_dict={"Water":self.Water,"Milk":self.Milk,"Coffee":self.Coffee,"Money":self.Money}
         return Resorces_dict
     
 resorces_in_machine=Resorces(300,150,100,0)
 
-=======
-
-resorces_in_machine=Resorces(300,150,100,0)
->>>>>>> bfc342d8e9bffeb26f4f8fc6d3edcaf3af3dfbf3
 
 # Class for Drinks
 class Drinks:
@@ -24,18 +19,14 @@ class Drinks:
         self.Milk = Milk
         self.Coffee = Coffee
         self.Cost = Cost
-<<<<<<< HEAD
     
     def dict_func(self):
         Drinks_dict = {"Water":self.Water,"Milk":self.Milk,"Coffee":self.Coffee,"Cost":self.Cost}
         return Drinks_dict
     
-=======
-        
->>>>>>> bfc342d8e9bffeb26f4f8fc6d3edcaf3af3dfbf3
-espresso = Drinks(50,30,25,30)
-latte = Drinks(60,50,35,25.5)
-cappuccino = Drinks(55,45,30,40)
+espresso = Drinks(50,30,25,3)
+latte = Drinks(60,50,35,2.5)
+cappuccino = Drinks(55,45,30,4)
 
 
 # Print func
@@ -50,7 +41,6 @@ Menu = {
 }
 
 # function for comparing resources
-<<<<<<< HEAD
 def is_resource_enough(drink):
     dint_reqs = drink.dict_func()
     resorces_have = resorces_in_machine.dict_func()
@@ -66,35 +56,21 @@ def is_resource_enough(drink):
       
 # Money func
 def process_coins():
-    pass      
-=======
-def compare():
-    pass
+   coins_quarters = int(input("How many quarters u will input: "))
+   coins_dimes = int(input("How many dimes u will input: "))
+   coins_nickles = int(input("How many nickles u will input: "))
+   coins_pennies = int(input("How many pennies u will input: "))
+   total_money = round(coins_quarters*0.25 + coins_dimes*0.10 + coins_nickles*0.05 + coins_pennies*0.01, 2)
+   return total_money
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> bfc342d8e9bffeb26f4f8fc6d3edcaf3af3dfbf3
+#Make coffee func & changes in the resources 
+def make_coffee(drink):
+    drink_reqs = drink.dict_func()
+    resorces_in_machine.Water -= drink_reqs["Water"]
+    resorces_in_machine.Milk -= drink_reqs["Milk"]
+    resorces_in_machine.Coffee -= drink_reqs["Coffee"]
+    resorces_in_machine.Money += drink.Cost
+    print("Here is your {}. Enjoy!".format(choice.lower()))
 
 #Loop for the program
 while True:
@@ -104,12 +80,16 @@ while True:
     elif choice.lower() == "off":
         break
     elif choice.lower()  in Menu:
-<<<<<<< HEAD
        chosen_drink=Menu[choice.lower()]
        enough=is_resource_enough(chosen_drink)
        if enough: 
-           print("The resources is enough to make ur order")
-=======
-        pass
-
->>>>>>> bfc342d8e9bffeb26f4f8fc6d3edcaf3af3dfbf3
+        print("The resources is enough to make ur order")
+        money_come = process_coins()
+        if money_come > chosen_drink.Cost:
+            remaining = money_come - chosen_drink.Cost
+            print("Here is ${} in change.".format(remaining))
+            make_coffee(chosen_drink)
+        elif money_come < chosen_drink.Cost:
+            print("Sorry that's not enough money. Money refunded.")
+        else:
+            make_coffee(chosen_drink)
